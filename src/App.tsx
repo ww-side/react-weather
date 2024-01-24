c85a03c56426879da0fd3e349432a3b622be6944
ww_side <daniil.metelia@gmail.com>
2024-01-24 19:55:08 +0200

    feat: add react-hot-toast to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Toaster } from 'react-hot-toast';
+import Form from './components/common/Form';
+import WeatherList from './components/common/WeatherList';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <main>
+      <Form />
+      <WeatherList />
+      <Toaster position="bottom-left" reverseOrder={false} />
+    </main>
+  );
 };
 
 export default App;
