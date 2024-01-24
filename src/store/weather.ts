e61e803d74ecf13586e21ba827c236d27472851e
ww_side <daniil.metelia@gmail.com>
2024-01-24 19:54:55 +0200

    feat: add mobx store

---
+++ src/store/weather.ts
@@ -0,0 +1,28 @@
+import { makeAutoObservable } from 'mobx';
+import type { WeatherDataType } from '../types/weather';
+
+interface WeatherModel {
+  weathers: WeatherDataType[];
+  addWeather(weather: WeatherDataType): void;
+  deleteWeather(id: number): void;
+}
+
+class Weather implements WeatherModel {
+  weathers: WeatherDataType[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addWeather = (weather: WeatherDataType) => {
+    this.weathers.push(weather);
+  };
+
+  deleteWeather = (id: number) => {
+    this.weathers = this.weathers.filter(weather => weather.id !== id);
+  };
+}
+
+const weatherStore = new Weather();
+
+export default weatherStore;
