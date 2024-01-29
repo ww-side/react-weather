467b0a8314b9f85ed4a91127533e6ffa9e56ca1f
ww_side <daniil.metelia@gmail.com>
2024-01-29 20:50:06 +0200

    fix: add https to weekly req

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -17,7 +17,7 @@ class WeatherService {
     try {
       return await axios
         .get(
-          `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}`
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}`
         )
         .then(res => res.data);
     } catch (err: any) {
