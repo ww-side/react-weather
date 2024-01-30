7db8076582bf33661cebbfe2c166c131b47d9777
ww_side <daniil.metelia@gmail.com>
2024-01-30 13:07:54 +0200

    refactor: update convert temperature

--- src/helpers/convertTemperature.ts
+++ src/helpers/convertTemperature.ts
@@ -1,7 +1,7 @@
 export const convertTemperature = (temp: number, toCelsius: boolean) => {
   if (toCelsius) {
-    return Math.round(temp - 273.15).toString();
+    return (temp - 273.15).toFixed(0).toString();
   } else {
-    return Math.round(1.8 * (temp - 273) + 32).toString();
+    return ((temp - 273.15) * (9 / 5) + 32).toFixed(0).toString();
   }
 };
